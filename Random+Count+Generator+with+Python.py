9a7faa01e932ab98f826cfeca51c0a282fad7876
neohann <mr.neohan@gmail.com>
2017-12-11 21:43:38 -0800

    v4

--- Random+Count+Generator+with+Python.py
+++ Random+Count+Generator+with+Python.py
@@ -1,24 +1,29 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[5]:
 
 import random
 import datetime
 import numpy as np
+import Queue
+import threading
 
 class Q1:
   def __init__(self):
     self.lst = []
     self.freq = {}
+    self.q = Queue.Queue()
     self.output = []
-    
+
   def generater(self):
-    self.lst = np.random.choice(np.arange(1, 6), 100, p = [0.5, 0.25, 0.15, 0.05, 0.05])
+    for num in np.random.choice(np.arange(1, 6), 100, p = [0.5, 0.25, 0.15, 0.05, 0.05]):
+      self.q.put(num)
+      self.lst.append(num)
     return self.lst
-    
+
   def frequency(self):
-    for num in self.lst:  
+    for num in self.lst:
       if num not in self.freq:
         self.freq[num] = 0.01
       else:
@@ -26,13 +31,16 @@ class Q1:
     return self.freq
 
   def writer(self):
-    self.output.append(str(self.lst[-1]) + " --> " + str(datetime.datetime.now()))
+    while not self.q.empty():
+      t = threading.Thread(target = self.generater, args = (self))
+      t.start()
+      self.output.append(str(self.q.get()) + " --> " + str(datetime.datetime.now()))
     return self.output
-    
+
 q1 = Q1()
-print q1.generater()
-print q1.frequency()
-print q1.writer()    
+q1.generater()
+#print q1.frequency()
+print q1.writer()
 
 
 # In[ ]:
