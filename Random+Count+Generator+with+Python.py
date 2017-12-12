35b2046573d9ce77fc9449741e41ba249a0c2d78
neohann <mr.neohan@gmail.com>
2017-12-11 21:40:53 -0800

    v3

--- Random+Count+Generator+with+Python.py
+++ Random+Count+Generator+with+Python.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[3]:
 
 import random
 import datetime
@@ -11,6 +11,7 @@ class Q1:
   def __init__(self):
     self.lst = []
     self.freq = {}
+    self.output = []
     
   def generater(self):
     self.lst = np.random.choice(np.arange(1, 6), 100, p = [0.5, 0.25, 0.15, 0.05, 0.05])
@@ -24,9 +25,14 @@ class Q1:
         self.freq[num] += 0.01
     return self.freq
 
+  def writer(self):
+    self.output.append(str(self.lst[-1]) + " --> " + str(datetime.datetime.now()))
+    return self.output
+    
 q1 = Q1()
 print q1.generater()
 print q1.frequency()
+print q1.writer()    
 
 
 # In[ ]:
